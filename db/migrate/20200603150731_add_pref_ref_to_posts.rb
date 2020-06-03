f8757fb0d5b14a5a26b5f4077eaaa103140ecdc9
Hijikata1869 <liv.knk@gmail.com>
2020-06-04 00:09:14 +0900

    ポストモデルに都道府県モデルを関連付け

---
+++ db/migrate/20200603150731_add_pref_ref_to_posts.rb
@@ -0,0 +1,5 @@
+class AddPrefRefToPosts < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :posts, :prefecture, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_144719) do
+ActiveRecord::Schema.define(version: 2020_06_03_150731) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "category"
@@ -25,8 +25,16 @@ ActiveRecord::Schema.define(version: 2020_06_03_144719) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id"
+    t.bigint "prefecture_id"
     t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["prefecture_id"], name: "index_posts_on_prefecture_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "prefecture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -43,5 +51,6 @@ ActiveRecord::Schema.define(version: 2020_06_03_144719) do
   end
 
   add_foreign_key "posts", "categories"
+  add_foreign_key "posts", "prefectures"
   add_foreign_key "posts", "users"
 end
