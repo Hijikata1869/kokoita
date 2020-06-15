61831cf2f68037e3671d635ceff27b853a5b1215
Hijikata1869 <liv.knk@gmail.com>
2020-06-15 23:40:36 +0900

    Postsコントローラを追加

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def create
+
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    
+  end
+
+end
