b2643e8f7932e0824e1cc4c584f3d84f7bcdee21
Hijikata1869 <liv.knk@gmail.com>
2020-06-28 07:12:53 +0900

    プロフィール画像の削除機能を登録情報変更ページに実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :picture, :picture_cache])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :picture, :picture_cache])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :picture, :picture_cache, :remove_picture])
   end
 
 end
