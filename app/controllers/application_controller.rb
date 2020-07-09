be28684128b0ca285dc3687a5006729c727f552f
Hijikata1869 <liv.knk@gmail.com>
2020-07-09 22:36:32 +0900

    投稿詳細ページのデザインを修正#4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :picture, :picture_cache, :remove_picture])
   end
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
 end
