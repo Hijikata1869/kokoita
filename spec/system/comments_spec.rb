60e1583c5462583682c2017e1cc32e9c9a3f42f6
Hijikata1869 <liv.knk@gmail.com>
2020-09-20 22:37:52 +0900

    ポストのシステムスペックに削除のシナリオを追加

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Comments", type: :system do
 
-  scenario "ユーザはポストにコメントをする" do
+  scenario "ログインユーザはポストにコメントをする" do
     user = FactoryBot.create(:user)
     post = FactoryBot.create(:post)
-    comment = FactoryBot.create(:comment)
 
     visit root_path
     click_link "ログイン"
@@ -15,15 +14,37 @@ RSpec.describe "Comments", type: :system do
 
     expect {
       click_link "詳しく見る", match: :first
-      fill_in "コメントを入力", with: "可愛いですね！"
+      fill_in "コメントを入力", with: "コメント"
       click_button "コメントする"
 
       expect(page).to have_content "コメントしました"
-      expect(page).to have_content "可愛いですね！"
+      expect(page).to have_content "コメント"
       expect(page).to have_content "このコメントを削除する"
-      save_and_open_page
     }.to change(user.comments, :count).by(1)
 
+  end
+
+  scenario "ログインユーザはコメントを削除できる" do
+    user = FactoryBot.create(:user)
+    post = FactoryBot.create(:post)
+
+    visit root_path
+    click_link "ログイン"
+    fill_in "Eメール", with: user.email
+    fill_in "パスワード", with: user.password
+    click_button "ログイン"
+
+    
+    click_link "詳しく見る", match: :first
+    fill_in "コメントを入力", with: "コメント"
+    click_button "コメントする"
+
+    expect {
+      click_link "このコメントを削除する"
+      expect(page).to have_content "コメントを削除しました"
+      expect(page).to have_content "投稿詳細"
+      expect(page).to have_content "#{post.found_animal}"
+    }.to change(user.comments, :count).by(-1)
   end
 
 end
--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -27,4 +27,31 @@ RSpec.describe "Posts", type: :system do
     }.to change(user.posts, :count).by(1)
   end
 
+  scenario "ユーザはポストを削除できる" do
+    user = FactoryBot.create(:user)
+    post = FactoryBot.create(:post)
+
+    visit root_path
+    click_link "ログイン"
+    fill_in "Eメール", with: user.email
+    fill_in "パスワード", with: user.password
+    click_button "ログイン"
+
+    click_link "投稿する"
+    fill_in "found_animal", with: post.found_animal
+    select '動物', from: 'category_id', match: :first
+    select '北海道', from: 'prefecture_id', match: :first
+    select '2019', from: '[found_date(1i)]'
+    select '6', from: '[found_date(2i)]'
+    click_button "投稿する"
+
+    expect {
+      click_link "削除する"
+
+      expect(page).to have_content "記事を一件削除しました"
+      expect(page).to have_content user.nickname
+      expect(page).to have_content "#{user.nickname}の投稿"
+    }.to change(user.posts, :count).by(-1)
+  end
+
 end
