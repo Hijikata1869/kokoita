e41862449d18d70c74da6781888442882d374bd7
Hijikata1869 <liv.knk@gmail.com>
2020-10-13 08:09:02 +0900

    carrierwave.rbを修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,13 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'image.kokoita.net'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: '<%= Rails.application.credentials.s3[:aws_access_key_id] %>',
-      aws_secret_access_key: '<%= Rails.application.credentials.s3[:aws_secret_access_key] %>',
+      aws_access_key_id: Rails.application.credentials.s3[:aws_access_key_id],
+      aws_secret_access_key: Rails.application.credentials.s3[:aws_secret_access_key],
       region: 'ap-northeast-1',
       path_style: true
     }
+    config.fog_directory  = 'image.kokoita.net'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/image.kokoita.net'
 end 
