04c0f4fac3c8eae661485debc9986bb4661e5857
Hijikata1869 <liv.knk@gmail.com>
2020-11-19 20:44:34 +0900

    config/unicorn/production.rbを修正

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -6,7 +6,7 @@ $app_dir = "/var/www/kokoita/current"
 
 $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
 
-$pid     = File.expend_path 'tmp/pids/unicorn.pid', $app_dir
+$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
 
 $std_log = File.expand_path 'log/unicorn.log', $app_dir
 
