2bf39b94d379a1dd63e1effc56bbf6ada18797d2
Hijikata1869 <liv.knk@gmail.com>
2021-01-14 14:27:25 +0900

    slider.jsを修正

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,69 +1,66 @@
 document.addEventListener("turbolinks:load", function(){
 
-  const slider = function(){
-    const prevBtn = document.getElementById('prev-btn');
-    const nextBtn = document.getElementById('next-btn');
-    prevBtn.style.display = "none";
 
-    const sliderWidth = document.querySelector('.slider-list');
-    let width = sliderWidth.clientWidth;
+  const prevBtn = document.getElementById('prev-btn');
+  const nextBtn = document.getElementById('next-btn');
+  prevBtn.style.display = "none";
 
-    const sliderList = document.querySelector('.slider-list');
-    const sliderItem = document.querySelectorAll('.slider-item');
+  const sliderWidth = document.querySelector('.slider-list');
+  let width = sliderWidth.clientWidth;
 
-    const indicator = document.querySelector('.indicator').children;
+  const sliderList = document.querySelector('.slider-list');
+  const sliderItem = document.querySelectorAll('.slider-item');
 
-    let counter = 0;
+  const indicator = document.querySelector('.indicator').children;
 
-    const firstIndex = 0;
-    const secondIndex = 1;
-    const thirdIndex = 2;
+  let counter = 0;
 
-    nextBtn.addEventListener('click', function() {
-      if(counter == sliderItem.length - 1) return;
-      prevBtn.style.display = "block";
-      sliderList.style.transition = ".5s";
-      counter ++;
-      sliderList.style.transform = "translateX("+ (- width * counter) + "px)";
-      if(counter == sliderItem.length - 2){
-        indicator.item(firstIndex).removeAttribute('id');
-        indicator.item(secondIndex).setAttribute('id', 'active');
-      } else if (counter == sliderItem.length -1){
-        indicator.item(secondIndex).removeAttribute('id');
-        indicator.item(thirdIndex).setAttribute('id', 'active');
+  const firstIndex = 0;
+  const secondIndex = 1;
+  const thirdIndex = 2;
+
+  nextBtn.addEventListener('click', function() {
+    if(counter == sliderItem.length - 1) return;
+    prevBtn.style.display = "block";
+    sliderList.style.transition = ".5s";
+    counter ++;
+    sliderList.style.transform = "translateX("+ (- width * counter) + "px)";
+    if(counter == sliderItem.length - 2){
+      indicator.item(firstIndex).removeAttribute('id');
+      indicator.item(secondIndex).setAttribute('id', 'active');
+    } else if (counter == sliderItem.length -1){
+      indicator.item(secondIndex).removeAttribute('id');
+      indicator.item(thirdIndex).setAttribute('id', 'active');
+    }
+
+    sliderList.addEventListener("transitionend", function(){
+      if(counter == sliderItem.length - 1){
+        sliderList.style.transition = "none";
+        nextBtn.style.display = "none";
       }
+    })
+  });
 
-      sliderList.addEventListener("transitionend", function(){
-        if(counter == sliderItem.length - 1){
-          sliderList.style.transition = "none";
-          nextBtn.style.display = "none";
-        }
-      })
-    });
+  prevBtn.addEventListener('click', function() {
+    if(counter == sliderItem.length - sliderItem.length) return;
+    nextBtn.style.display = "block";
+    sliderList.style.transition = ".5s";
+    counter --;
+    sliderList.style.transform = "translateX("+ (- width * counter) + "px)";
+    if(counter == sliderItem.length - 2){
+      indicator.item(thirdIndex).removeAttribute('id');
+      indicator.item(secondIndex).setAttribute('id', 'active');
+    } else if (counter == sliderItem.length - sliderItem.length){
+      indicator.item(secondIndex).removeAttribute('id');
+      indicator.item(firstIndex).setAttribute('id','active');
+    }
 
-    prevBtn.addEventListener('click', function() {
-      if(counter == sliderItem.length - sliderItem.length) return;
-      nextBtn.style.display = "block";
-      sliderList.style.transition = ".5s";
-      counter --;
-      sliderList.style.transform = "translateX("+ (- width * counter) + "px)";
-      if(counter == sliderItem.length - 2){
-        indicator.item(thirdIndex).removeAttribute('id');
-        indicator.item(secondIndex).setAttribute('id', 'active');
-      } else if (counter == sliderItem.length - sliderItem.length){
-        indicator.item(secondIndex).removeAttribute('id');
-        indicator.item(firstIndex).setAttribute('id','active');
+    sliderList.addEventListener("transitionend", function(){
+      if(counter == sliderItem.length - sliderItem.length){
+        sliderList.style.transition = "none";
+        prevBtn.style.display = "none";
       }
-
-      sliderList.addEventListener("transitionend", function(){
-        if(counter == sliderItem.length - sliderItem.length){
-          sliderList.style.transition = "none";
-          prevBtn.style.display = "none";
-        }
-      })
-    });
-  };
-
-  slider();
+    })
+  });
 
 });
