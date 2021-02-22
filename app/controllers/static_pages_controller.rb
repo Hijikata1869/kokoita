c41fc8fcaa7cba03b7b5b7673ec959ab2a5127fa
Hijikata1869 <liv.knk@gmail.com>
2021-02-23 06:07:09 +0900

    フォロー・フォロワー機能に関連するページをレスポンシブ化#23

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,7 @@ class StaticPagesController < ApplicationController
     @ranks = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
     @danger_tag_posts = Post.joins(:tags).where('tag_name like ?', '%危険%').order('created_at desc').limit(5)
     if user_signed_in?
-      @following_user_posts = Post.where(user_id: [current_user.following_ids]).limit(5)
+      @following_user_posts = Post.where(user_id: [current_user.following_ids]).order('created_at desc').limit(5)
     end
   end
 
