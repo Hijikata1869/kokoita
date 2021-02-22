38dc11e703f2afa21b2b120d218a89ce8711d040
Hijikata1869 <liv.knk@gmail.com>
2021-02-22 18:44:02 +0900

    フォローしているユーザの投稿を取得、表示#21

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
     @posts = Post.page(params[:page]).order('created_at DESC')
     @ranks = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
     @danger_tag_posts = Post.joins(:tags).where('tag_name like ?', '%危険%').order('created_at desc').limit(5)
+    @following_user_posts = Post.where(user_id: [current_user.following_ids])
   end
 
   def help; end
