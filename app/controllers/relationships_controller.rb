3efcd63fae439e4a0ebf0c6f010e5814281b844d
Hijikata1869 <liv.knk@gmail.com>
2021-02-25 06:11:40 +0900

    ユーザ詳細ページのフォロー・フォロー解除ボタンをAjax化#23

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,12 +1,11 @@
 class RelationshipsController < ApplicationController
   def create
-    follow = current_user.active_relationships.build(follower_id: params[:user_id])
-    user = User.find(params[:user_id])
-    follow.save
+    @user = User.find(params[:user_id])
+    current_user.follow(@user)
   end
 
   def destroy
-    follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
-    follow.destroy
+    @user = User.find(params[:user_id])
+    current_user.unfollow(@user)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,17 +11,25 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   # 自分がフォローしているユーザとの関係
-  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id, dependent: :destroy
   has_many :followings, through: :active_relationships, source: :follower
 
   # 自分がフォローされるユーザとの関係
-  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :following
 
   validates :nickname, presence: true, length: { minimum: 2, maximum: 30 }
 
-  def followed_by?(user)
-    passive_relationships.find_by(following_id: user.id).present?
+  def following?(user)
+    active_relationships.find_by(follower_id: user.id)
+  end
+
+  def follow(user)
+    active_relationships.create!(follower_id: user.id)
+  end
+
+  def unfollow(user)
+    active_relationships.find_by(follower_id: user.id).destroy
   end
 
   def self.guest
